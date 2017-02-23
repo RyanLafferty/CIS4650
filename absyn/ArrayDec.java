42d08cb5e4c9859fb0586593157e7ade3c0a4a08
Christopher Katsaras <ckatsara@mail.uoguelph.ca>
2017-02-22 20:07:05 -0500

    Once again, making use of structures Fei has given to us

--- absyn/ArrayDec.java
+++ absyn/ArrayDec.java
@@ -4,9 +4,9 @@ public class ArrayDec extends Exp {
 
   public TypeSpec type;
   public String id;
-  public String number;
+  public IntExp number;
 
-  public ArrayDec(int pos, TypeSpec type, String id, String number) {
+  public ArrayDec(int pos, TypeSpec type, String id, IntExp number) {
   	this.pos = pos;
   	this.type = type;
   	this.id = id;
