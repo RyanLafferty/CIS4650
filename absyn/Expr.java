9793b9ced7838ccd06f1ed0808d0f8196317bcf3
RyanLafferty <rlafferty1@hotmail.com>
2017-02-26 18:51:24 -0500

    fixed minor issues

--- absyn/Expr.java
+++ absyn/Expr.java
@@ -1,10 +1,10 @@
 package absyn;
 
 public class Expr extends Dec {
-  public Var var;
+  public VarDec var;
   public Dec expression;
   public Dec sime;
-  public Expr( int pos, Var var, Dec expression) {
+  public Expr( int pos, VarDec var, Dec expression) {
     this.pos = pos;
     this.var = var;
     this.expression = expression;
