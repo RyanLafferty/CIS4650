1cb180af194e884287d4c422396868171778f41c
Christopher Katsaras <ckatsara@mail.uoguelph.ca>
2017-03-20 14:10:44 -0400

    Added constructor for regularDec. Helps us identify arrays

--- absyn/RegularDec.java
+++ absyn/RegularDec.java
@@ -4,10 +4,18 @@ public class RegularDec extends VarDec {
 
   public TypeSpec type;
   public String id;
+  public int size;
 
   public RegularDec(int pos, TypeSpec type, String id) {
   	this.pos = pos;
   	this.type = type;
   	this.id = id;
+  	this.size = 0;
+  }
+  public RegularDec(int pos, TypeSpec type, String id, int size) {
+  	this.pos = pos;
+  	this.type = type;
+  	this.id = id;
+  	this.size = size;
   }
 }
